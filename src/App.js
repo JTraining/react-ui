f7b307a59fbd8d1ae2856ffc0ccbae9426e638f8
JTraining <37625000+JTraining@users.noreply.github.com>
2018-03-21 17:42:02 +0300

    First commit
    First commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+//import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+//
+//import Form from './components/Form';
+//
+//class App extends Component {
+//  render() {
+//    return (
+//      <div className="App">
+//        <Form />
+//    
+//      </div>
+//    );
+//  }
+//}
+//
+//export default App;
+
+
+import React, { Component } from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+
+import './App.css';
+import Form from './components/Form';
+
+class App extends Component {
+    state = {
+        mounted: true
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ mounted: false });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <CSSTransitionGroup
+                    transitionName="fade"
+                    transitionAppear={true}
+                    transitionAppearTimeout={5000}
+                    transitionEnter={false}
+                    transitionLeaveTimeout={300}>
+                    {this.state.mounted && <Form onSubmit={this.handleSubmit} />}
+                </CSSTransitionGroup>
+            </div>
+        );
+    }
+}
+
+export default App;
